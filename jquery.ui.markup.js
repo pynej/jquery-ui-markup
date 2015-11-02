5416ce3819d63e62487445d4ca9a271295fed16f
Jeremy Pyne <jeremy.pyne@gmail.com>
2015-11-02 10:54:46 -0500

    Update jquery.ui.markup.js
    Added additional logic for data-ui="dialog" to runt he code when the element is clicked and not on page load.  There would be no reason to dun the dialog code for all elements on initial lode like normal happens.  This could probably be expanded with a attribute with more global options.  Ie: data-ui-load="load/click/other event".
    
    Also a data-ui-target="class  selector" was added to specify the target of a dialog.  In this code the dialog options are on the button triggering it, not the actually dialog html code/element.  This is so that one element can have more then one configuration.

--- jquery.ui.markup.js
+++ jquery.ui.markup.js
@@ -1,48 +1,83 @@
-(function( $ ) {
+(function ($) {
 
-var namespace = "data-ui",
-	optionsNamespace = namespace+"-";
+    var namespace = "data-ui",
+        optionsNamespace = namespace + "-";
 
-function attrToOption( attr ) {
-	attr = attr.substring( optionsNamespace.length ).toLowerCase();
-	return attr.replace( /-.?/g, function( string ) {
-		return string.charAt( 1 ).toUpperCase();
-	});
-}
+    function attrToOption(attr) {
+        attr = attr.substring(optionsNamespace.length).toLowerCase();
+        return attr.replace(/-.?/g, function (string) {
+            return string.charAt(1).toUpperCase();
+        });
+    }
 
-function callUI( element, ui, options ) {
-	element[ ui ]( options );
-}
-
-$( document ).ready(function() {
-	$( "["+namespace+"]" ).each(function() {
-		var i, name, options = {}, value,
-			$this = $( this ),
-			ui = $this.data("ui");
-
-		for ( i = 0; i < this.attributes.length; i++ ) {
-			name = this.attributes[i].name;
-			value = this.attributes[i].value;
-
-			if ( name.indexOf( optionsNamespace ) === 0 && name.length > optionsNamespace.length ) {
-				if ( /(true|false)/i.test( value ) ) {
+    function callUI(element, ui, options) {
+        element[ui](options);
+    }
+	
+	function prepUI(element, ui, options) {
+		var i, name, value;
+		
+		for (i = 0; i < element[0].attributes.length; i++) {
+			name = element[0].attributes[i].name;
+			value = element[0].attributes[i].value;
+			
+			if (name.indexOf(optionsNamespace) === 0 && name.length > optionsNamespace.length) {
+				if (/(true|false)/i.test(value)) {
 					value = value.toLowerCase() === "true";
-				} else if ( !isNaN( value ) ) {
-					value = +value;	
-				} else if ( value.charAt(0) === "{" ) {
-					value = eval("("+value+")");
+				} else if (!isNaN(value)) {
+					value = +value;
+				} else if (value.charAt(0) === "{") {
+					value = eval("(" + value + ")");
 				}
-
-				options[ attrToOption( name ) ] = value;
+				
+				if(attrToOption(name) == "slide")
+					options[attrToOption(name)] = function(event, ui) {
+						var value = $(event.target).data("ui-slide");
+						if($(value).is("input"))
+							$(value).val(ui.value).change();
+						else
+							$(value).text(ui.value);
+					};
+				else if(attrToOption(name) == "change")
+					options[attrToOption(name)] = function(event, ui) {
+						var value = $(event.target).data("ui-change");
+						if($(value).is("input"))
+							$(value).val(ui.value).change();
+						else
+							$(value).text(ui.value);
+					};
+				else
+					options[attrToOption(name)] = value;
 			}
 		}
-
-		if ( /(addClass|effect|hide|show|toggle)/i.test( ui ) && options.delay ) {
-			window.setTimeout(function() { callUI( $this, ui, options ); }, options.delay );
+		
+		if(ui == 'dialog')
+			$this = $(element.data("ui-target"));
+		
+		
+		if (/(addClass|effect|hide|show|toggle)/i.test(ui) && options.delay) {
+			window.setTimeout(function () { callUI($this, ui, options); }, options.delay);
 		} else {
-			callUI( $this, ui, options );
+			callUI($this, ui, options);
 		}
-	});
-});
+	}
 
-}( jQuery ) );
+    $(document).ready(function () {
+        $("[" + namespace + "]").each(function () {
+            var $this = $(this), options = {},
+                ui = $this.data("ui");
+			
+			if(ui == 'dialog')
+				$(this).click(function() {
+					prepUI($this, ui, options);
+					if($(this).is("a[href!='#']"))
+						return false;
+				});
+			else
+				prepUI($this, ui, options);
+			
+            
+        });
+    });
+
+}(jQuery));
